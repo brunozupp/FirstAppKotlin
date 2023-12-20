40d8288a7742c8b6adfab48a085a4ad5ac509ee6
Bruno Noveli <brunimzuppwolf@gmail.com>
2023-12-20 13:52:48 -0300

    Layout da tela + mudança no Compile SDK

--- app/src/main/java/com/novelitech/firstappkotlin/MainActivity.kt
+++ app/src/main/java/com/novelitech/firstappkotlin/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.novelitech.firstappkotlin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,31 +17,41 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.novelitech.firstappkotlin.ui.theme.FirstAppKotlinTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var result: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FirstAppKotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android Noveli")
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        result = findViewById<TextView>(R.id.txt_result)
+
+        val buttonConverter = findViewById<Button>(R.id.btn_converter)
+
+        buttonConverter.setOnClickListener {
+            converter()
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    private fun converter() {
+        val selectedCurrency = findViewById<RadioGroup>(R.id.radio_group)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FirstAppKotlinTheme {
-        Greeting("Android")
+        val checked = selectedCurrency.checkedRadioButtonId
+
+        val currency = when(checked) {
+            R.id.radio_usd -> "USD"
+            R.id.radio_eur -> "EUR"
+            else -> "CLP"
+        }
+
+        val editField = findViewById<EditText>(R.id.edit_field)
+
+        val value = editField.text.toString()
+
+        if(value.isEmpty()) {
+            return
+        } else {
+
+        }
     }
 }
